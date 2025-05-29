8eca1263da2bef161161cba0009e1825b7cadcad
CamMV <j.murciav@uniandes.edu.co>
2025-05-29 09:20:02 -0500

    a

--- Diagnosticos/diagnosticos/models.py
+++ Diagnosticos/diagnosticos/models.py
@@ -3,12 +3,20 @@ from mongoengine.fields import StringField, DateTimeField, ReferenceField, ListF
 
 # Create your models here.
 class Diagnostico(Document):
+    meta = {
+        'db_alias': 'main',
+        'collection': 'diagnosticos',
+    }
     resultado = StringField(required=True)
     diagnostico = StringField(required=True)
     fecha_diagnostico = DateTimeField(required=True)
     version = StringField(required=True)
     
 class IntentoDiagnostico(Document):
+    meta = {
+        'db_alias': 'main',
+        'collection': 'intentos_diagnosticos'
+    }
     diagnostico = ReferenceField(Diagnostico, required=True)
     fecha_intento = DateTimeField(required=True)
     cambio = StringField(required=True)    
