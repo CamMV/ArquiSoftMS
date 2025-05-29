ae7ffcfd77d27694edbca0e7026f5f1c7433431f
CamMV <j.murciav@uniandes.edu.co>
2025-05-29 09:08:07 -0500

    a

--- Diagnosticos/diagnosticos/urls.py
+++ Diagnosticos/diagnosticos/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     # Autores
-    path('diagnosticos/', views.diagnosticos_list, name='diagnosticos_list'),
-    path('diagnosticos/<str:diagnostico_id>/', views.diagnostico_detail, name='diagnostico_detail'),
-    path('diagnosticos/nuevo/', views.diagnostico_create, name='diagnostico_create'),
+    path('', views.diagnosticos_list, name='diagnosticos_list'),
+    path('<str:diagnostico_id>/', views.diagnostico_detail, name='diagnostico_detail'),
+    path('/uevo/', views.diagnostico_create, name='diagnostico_create'),
 
     # Publicaciones
-    path('diagnosticos/intentos', views.intentos_diagnosticos_list, name='intentos_diagnosticos_list'),
-    path('diagnosticos/<str:diagnostico_id>/', views.intento_diagnostico, name='diagnostico_intento'),
+    path('intentos', views.intentos_diagnosticos_list, name='intentos_diagnosticos_list'),
+    path('<str:diagnostico_id>/', views.intento_diagnostico, name='diagnostico_intento'),
 ]
