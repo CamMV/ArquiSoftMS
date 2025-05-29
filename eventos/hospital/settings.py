e21d43665d94bd5406b584f7fca1b59debeb7e8e
CamMV <j.murciav@uniandes.edu.co>
2025-05-29 04:53:55 -0500

    a

--- eventos/hospital/settings.py
+++ eventos/hospital/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'hospital.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [[os.path.join(BASE_DIR, 'hospital', 'templates')]],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
