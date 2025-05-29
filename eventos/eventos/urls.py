dd3147c4368be94c2483cffdda30314a2327502e
CamMV <j.murciav@uniandes.edu.co>
2025-05-29 04:32:33 -0500

    a

--- eventos/eventos/urls.py
+++ eventos/eventos/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import routers
 
 from . import views
 
+router = routers.DefaultRouter()
+
+router.register(r'eventos', views.EventoViewSet, basename='evento')
+
 urlpatterns = [
-    path('', views.eventoList, name = 'eventoList'),
-    path('eventoCreate/', csrf_exempt(views.evento_create), name = 'eventoCreate'),
+    path('', include(router.urls)),
+    path('create/', csrf_exempt(views.EventoViewSet.as_view({'post': 'create'})), name='evento-create'),
 ]
