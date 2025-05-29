cab86c0842059c39025065421fcca1aaa6b4ddd3
CamMV <j.murciav@uniandes.edu.co>
2025-05-29 04:37:03 -0500

    a

--- eventos/hospital/settings.py
+++ eventos/hospital/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'eventos.urls'
+ROOT_URLCONF = 'hospital.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'eventos.wsgi.application'
+WSGI_APPLICATION = 'hospital.wsgi.application'
 
 
 # Database
