5aa8e08f4ca01d1fb2e76c20b42fe9e7aece3d0d
CamMV <j.murciav@uniandes.edu.co>
2025-05-29 02:26:00 -0500

    .

--- Pacientes/models/db.py
+++ Pacientes/models/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
@@ -11,18 +11,9 @@ DATABASE_URL = os.environ.get(
 
 engine = create_async_engine(DATABASE_URL, echo=True, future=True, pool_recycle=3600)
 
-AsyncSessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False # Esto evita que los objetos cargados expiren después de un commit
-)
+AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
 async def init_db():
-    """
-    Función para inicializar la base de datos (crear tablas si no existen).
-    """
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
